2aeb53f8bae041191f13514b494f5316f873fe6a
Rabie-s <asdfw.jsd@gmail.com>
2026-02-08 11:21:50 +0300

    fix auth sanctum

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   },
 
   sanctum: {
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://localhost:8000',
     endpoints: {
       login: '/api/v1/user/auth/login',
       logout: '/api/v1/user/logout',
